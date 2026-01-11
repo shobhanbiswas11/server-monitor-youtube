5d31645508e8e7e110b0553011151edd6301fe7f
Shobhan Biswas <61372335+shobhanbiswas11@users.noreply.github.com>
2026-01-11 10:10:37 +0530

    app module update

--- apps/nest-app/src/app.module.ts
+++ apps/nest-app/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
+import { RemoteServersModule } from './remote-servers/remote-servers.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { UsersModule } from './users/users.module';
       synchronize: true,
     }),
     UsersModule,
+    RemoteServersModule,
+    AuthModule,
   ],
   controllers: [AppController],
 })
