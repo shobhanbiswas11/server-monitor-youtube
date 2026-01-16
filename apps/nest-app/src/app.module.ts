ce5cf527470d9b4e5c8f868da94cb36ed775f937
Shobhan Biswas <61372335+shobhanbiswas11@users.noreply.github.com>
2026-01-17 00:32:22 +0530

    update app module

--- apps/nest-app/src/app.module.ts
+++ apps/nest-app/src/app.module.ts
@@ -1,22 +1,17 @@
 import { Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 import { LogAnalysisModule } from './log-analysis/log-analysis.module';
 import { LogSourcesModule } from './log-sources/log-sources.module';
 import { RemoteServersModule } from './remote-servers/remote-servers.module';
-import { UsersModule } from './users/users.module';
 import { TicketingModule } from './ticketing/ticketing.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'better-sqlite3',
-      database: 'db.sqlite',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    DatabaseModule,
     UsersModule,
     RemoteServersModule,
     AuthModule,
@@ -24,6 +19,7 @@ import { TicketingModule } from './ticketing/ticketing.module';
     LogAnalysisModule,
     EventEmitterModule.forRoot(),
     TicketingModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
 })
