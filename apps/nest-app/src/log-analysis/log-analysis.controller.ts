e3264068db224e10ffa50efdcef0f2a48f37035b
Shobhan Biswas <61372335+shobhanbiswas11@users.noreply.github.com>
2026-01-18 11:29:01 +0530

    sending 200 in log ingestion

--- apps/nest-app/src/log-analysis/log-analysis.controller.ts
+++ apps/nest-app/src/log-analysis/log-analysis.controller.ts
@@ -1,14 +1,15 @@
 import { CurrentUser } from '@/auth/current-user.decorator';
 import type { ICurrentUser } from '@/auth/current-user.interface';
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Param, Post } from '@nestjs/common';
 import { ApiBody } from '@nestjs/swagger';
 import { LogAnalysisService } from './log-analysis.service';
 
 @Controller('log-analysis')
 export class LogAnalysisController {
-  constructor(private readonly logAnalysisService: LogAnalysisService) {}
+  constructor(private readonly logAnalysisService: LogAnalysisService) { }
 
   @ApiBody({ type: Array<Record<string, any>> })
+  @HttpCode(200)
   @Post('ingest/:jobId')
   ingestLogs(
     @Param('jobId') jobId: string,
