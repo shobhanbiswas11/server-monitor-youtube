47cc5d503a013232abe330d9d6984d9a157764ec
Shobhan Biswas <61372335+shobhanbiswas11@users.noreply.github.com>
2026-02-25 08:12:23 +0530

    database test module entity explicit mention

--- apps/nest-app/src/database/database-test.module.ts
+++ apps/nest-app/src/database/database-test.module.ts
@@ -1,3 +1,6 @@
+import { Anomaly, LogAnalysisJob } from '@/log-analysis';
+import { LogSource } from '@/log-sources/entities/log-source.entity';
+import { RemoteServer } from '@/remote-servers';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
@@ -6,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'better-sqlite3',
       database: ':memory:',
-      autoLoadEntities: true,
+      entities: [Anomaly, LogAnalysisJob, RemoteServer, LogSource],
       synchronize: true,
       dropSchema: true,
     }),
